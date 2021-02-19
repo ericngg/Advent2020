aca5ff2967d161dcb8d65d6a4af8e92d57af39df
Eric Ng <EricNg109@gmail.com>
2021-02-19 11:38:24 -0800

    day 15 fix syntax

--- src/day 15/Day15.java
+++ src/day 15/Day15.java
@@ -4,12 +4,7 @@ import java.util.*;
 public class Day15 {
     public static void main(String[] args) {
         Map<Long, Long> map = new HashMap<>();
-        input(map, new long[]{Long.valueOf(20),
-                              Long.valueOf(0),
-                              Long.valueOf(1),
-                              Long.valueOf(11),
-                              Long.valueOf(6),
-                              Long.valueOf(3)});
+        input(map, new long[]{20L, 0L, 1L, 11L, 6L, 3L});
 
         System.out.println(solution(map));
 
@@ -21,18 +16,16 @@ public class Day15 {
         long lastIndex = map.size() + 1;
 
         for (long i = lastIndex + 1; i <= stop; i++) {
-            if (map.keySet().contains(last)) {
+            if (map.containsKey(last)) {
                 long tempLast = lastIndex - map.get(last);
                 map.put(last, lastIndex);
-
                 last = tempLast;
-                lastIndex = i;
             } else {
                 map.put(last, lastIndex);
-
                 last = 0;
-                lastIndex = i;
             }
+
+            lastIndex = i;
         }
 
         return last;
@@ -40,7 +33,7 @@ public class Day15 {
 
     public static void input(Map<Long, Long> map, long[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], Long.valueOf(i + 1));
+            map.put(arr[i], (long) (i + 1));
         }
     }
 }
